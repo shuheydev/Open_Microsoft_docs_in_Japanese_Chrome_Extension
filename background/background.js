a703e261f52130f94af8c867042ca562ceafd91f
shuheydev <shuhey.dev@gmail.com>
2020-09-24 16:52:23 +0900

    processing for only docs.microsoft.com

--- background/background.js
+++ background/background.js
@@ -7,6 +7,11 @@ browser.browserAction.onClicked.addListener(function () {
     browser.tabs.getSelected(null, function (tab) {
         let url = tab.url;
 
+        //https://docs.microsoft.com/に絞る
+        let re = /^https:\/\/docs.microsoft.com\/.*$/gi;
+        if (!re.exec(url))
+            return;
+
         //urlを別ウィンドウで開く
         browser.windows.create({ url: url });
     });
